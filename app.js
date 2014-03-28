46ee4d893ce46ccad8f54cb464a7c23078be39b2
Derek Erdmann <derek@derekerdmann.com>
2014-03-28 00:18:43 -0400

    Add automatic rebuilds with --watch flag

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var fs = require("fs");
 var Metalsmith = require("metalsmith");
 var sass = require("metalsmith-sass");
 var ignore = require("metalsmith-ignore");
+var watch = require('metalsmith-watch');
 
 var app = express();
 
@@ -30,12 +31,16 @@ if ('development' == app.get('env')) {
 }
 
 // Generate metalsmith content
-var ms = Metalsmith(__dirname)
-    .use(ignore( ".*" ))  // ignore temp files
-    .use(sass({ outputStyle: "expanded" }))
-    .build(function(err){
-        if (err) throw err;
-    });
+var ms = Metalsmith(__dirname);
+ms.use( ignore( ".*" ) );  // ignore temp files
+ms.use( sass({ outputStyle: "expanded" }) )
+
+// Allow automatic rebuilds
+if (process.argv.indexOf( "--watch" ) > -1 ) {
+    ms.use(watch)
+}
+
+ms.build(function(err){ if (err) throw err; });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
