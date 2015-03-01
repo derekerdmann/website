77023014614eabb0d40e31a980e4318a3ad9c32e
Derek Erdmann <derek@derekerdmann.com>
2015-03-01 14:53:22 -0800

    Update deploy script to clean public directory

--- deploy.cmd
+++ deploy.cmd
@@ -104,6 +104,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 4. Build site
+  rmdir public
   call :ExecuteCmd !NPM_CMD! run-script buildprod
   IF !ERRORLEVEL! NEQ 0 goto error
 
